5cd7944b0b680a75ae6f1c093d0e6c12b2d866a9
Vidhi Aggarwal <vidhiaggarwal397@gmail.com>
2022-03-29 23:21:30 +0530

    Rotate Array added

---
+++ src/com/dsa/Arrays/RotateArray.java
@@ -0,0 +1,43 @@
+package com.dsa.Arrays;
+/*
+Question: https://leetcode.com/problems/rotate-array
+Solution: https://leetcode.com/submissions/detail/669805586/
+Time Complexity: O(n)
+Space Complexity: O(1)
+ */
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int nums [] = {1,2,3,4,5,6,7};
+        int k = 3;
+        rotate(nums,k);
+        System.out.println(Arrays.toString(nums));
+
+    }
+    public static void  rotate(int[] nums, int k) {
+    int n = nums.length ;
+    k %=n;
+    if(k < 0 )
+        k += n;
+    //divide the array in two parts
+    //first part will be starting from first index till the kth part
+    //second part will be starting just from the kth to n-1
+    //in the last revers the whole array
+    reverse(nums, 0 , n-k-1);
+    reverse(nums, n-k, n-1);
+    reverse (nums,0,n-1);
+
+}
+    public static void  reverse ( int [] nums, int start, int end)
+    {
+        while(start<end)
+        {
+            nums[start] = nums[start]+nums[end];
+            nums[end] = nums[start]-nums[end];
+            nums[start] =  nums[start] - nums[end];
+            start++;end--;
+        }
+    }
+
+}
